0c104db3f9e6ebd96ce0f027e30040fd364426f7
THANHPC\Accou <nct031194@icloud.com>
2025-02-18 12:41:21 -0600

    Remove entire React front-end application setup.
    This commit removes all files related to the React front-end, including source code, configurations, dependencies, and build assets. The deleted files encompass project setup files, React components, styles, routing, and package manager configurations such as `package.json` and `package-lock.json`. This indicates a complete decommissioning or transition away from this front-end stack.

---
+++ src/App.tsx
@@ -0,0 +1,60 @@
+import { Route, BrowserRouter as Router, Routes } from "react-router";
+import AppLayout from "./layout/AppLayout";
+import AuthLayout from "./layout/AuthLayout";
+import SignIn from "./pages/AuthPages/SignIn";
+import SignUp from "./pages/AuthPages/SignUp";
+import Blank from "./pages/Blank";
+import Calendar from "./pages/Calendar";
+import Ecommerce from "./pages/Dashboard/ECommerce";
+import FormElements from "./pages/Forms/FormElements";
+import NotFound from "./pages/OtherPage/NotFound";
+import BasicTables from "./pages/Tables/BasicTables";
+import Alerts from "./pages/UiElements/Alerts";
+import Avatars from "./pages/UiElements/Avatars";
+import Badges from "./pages/UiElements/Badges";
+import Buttons from "./pages/UiElements/Buttons";
+import Images from "./pages/UiElements/Images";
+import Videos from "./pages/UiElements/Videos";
+import UserProfiles from "./pages/UserProfiles";
+
+export default function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          {/* Dashboard Layout */}
+          <Route element={<AppLayout />}>
+            <Route index path="/" element={<Ecommerce />} />
+            {/* Others Page */}
+            <Route path="/profile" element={<UserProfiles />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/blank" element={<Blank />} />
+
+            {/* Forms */}
+            <Route path="/form-elements" element={<FormElements />} />
+
+            {/* Tables */}
+            <Route path="/basic-tables" element={<BasicTables />} />
+
+            {/* Ui Elements */}
+            <Route path="/alerts" element={<Alerts />} />
+            <Route path="/avatars" element={<Avatars />} />
+            <Route path="/badges" element={<Badges />} />
+            <Route path="/buttons" element={<Buttons />} />
+            <Route path="/images" element={<Images />} />
+            <Route path="/videos" element={<Videos />} />
+          </Route>
+
+          {/* Auth Layout */}
+          <Route element={<AuthLayout />}>
+            <Route path="/signin" element={<SignIn />} />
+            <Route path="/signup" element={<SignUp />} />
+          </Route>
+
+          {/* Fallback Route */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
