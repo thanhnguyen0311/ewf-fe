9601214f0505ade542c9852f8e4b071d8562f2ce
ThanhNguyen0311 <nct031194@icloud.com>
2025-02-24 19:55:15 -0600

    Refactor and update UI structure in NewOrder component
    Adjusted formatting and indentation for better readability in NewOrder.tsx. Added new input fields for phone and general input, while removing redundant phone input fields. Updated workspace.xml with minor changes to component properties and markdown settings.

--- src/pages/Order/NewOrder.tsx
+++ src/pages/Order/NewOrder.tsx
@@ -99,8 +99,8 @@ export default function NewOrder() {
                                     />
                                     <span
                                         className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-              <CalenderIcon/>
-            </span>
+                                      <CalenderIcon/>
+                                    </span>
                                 </div>
                             </div>
                             <div>
@@ -114,8 +114,8 @@ export default function NewOrder() {
                                     />
                                     <span
                                         className="absolute text-gray-500 -translate-y-1/2 pointer-events-none right-3 top-1/2 dark:text-gray-400">
-              <TimeIcon/>
-            </span>
+                                      <TimeIcon/>
+                                    </span>
                                 </div>
                             </div>
                             <div>
@@ -128,21 +128,21 @@ export default function NewOrder() {
                                     />
                                     <span
                                         className="absolute left-0 top-1/2 flex h-11 w-[46px] -translate-y-1/2 items-center justify-center border-r border-gray-200 dark:border-gray-800">
-              <svg
-                  width="20"
-                  height="20"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-              >
-                <circle cx="6.25" cy="10" r="5.625" fill="#E80B26"/>
-                <circle cx="13.75" cy="10" r="5.625" fill="#F59D31"/>
-                <path
-                    d="M10 14.1924C11.1508 13.1625 11.875 11.6657 11.875 9.99979C11.875 8.33383 11.1508 6.8371 10 5.80713C8.84918 6.8371 8.125 8.33383 8.125 9.99979C8.125 11.6657 8.84918 13.1625 10 14.1924Z"
-                    fill="#FC6020"
-                />
-              </svg>
-            </span>
+                                      <svg
+                                          width="20"
+                                          height="20"
+                                          viewBox="0 0 20 20"
+                                          fill="none"
+                                          xmlns="http://www.w3.org/2000/svg"
+                                      >
+                                        <circle cx="6.25" cy="10" r="5.625" fill="#E80B26"/>
+                                        <circle cx="13.75" cy="10" r="5.625" fill="#F59D31"/>
+                                        <path
+                                            d="M10 14.1924C11.1508 13.1625 11.875 11.6657 11.875 9.99979C11.875 8.33383 11.1508 6.8371 10 5.80713C8.84918 6.8371 8.125 8.33383 8.125 9.99979C8.125 11.6657 8.84918 13.1625 10 14.1924Z"
+                                            fill="#FC6020"
+                                        />
+                                      </svg>
+                                    </span>
                                 </div>
                             </div>
                         </div>
@@ -158,6 +158,19 @@ export default function NewOrder() {
                     <ComponentCard title="Customer">
                         <div className="space-y-6">
                             <div>
+                                <Label>Phone</Label>
+                                <PhoneInput
+                                    selectPosition="start"
+                                    countries={countries}
+                                    placeholder="+1 (555) 000-0000"
+                                    onChange={handlePhoneNumberChange}
+                                />
+                            </div>
+                            <div>
+                                <Label>Input</Label>
+                                <Input type="text"/>
+                            </div>
+                            <div>
                                 <Label>Email</Label>
                                 <div className="relative">
                                     <Input
@@ -170,25 +183,6 @@ export default function NewOrder() {
                                       <EnvelopeIcon/>
                                     </span>
                                 </div>
-                            </div>
-                            <div>
-                                <Label>Phone</Label>
-                                <PhoneInput
-                                    selectPosition="start"
-                                    countries={countries}
-                                    placeholder="+1 (555) 000-0000"
-                                    onChange={handlePhoneNumberChange}
-                                />
-                            </div>
-                            {" "}
-                            <div>
-                                <Label>Phone</Label>
-                                <PhoneInput
-                                    selectPosition="end"
-                                    countries={countries}
-                                    placeholder="+1 (555) 000-0000"
-                                    onChange={handlePhoneNumberChange}
-                                />
                             </div>
                         </div>
                     </ComponentCard>
