e1a66e01a4453f62041ac10185bb274b08597617
THANHPC\Accou <nct031194@icloud.com>
2025-03-11 17:10:05 -0500

    Add sorting and enhancements to inventory components view
    This update introduces sorting of components based on a predefined SKU order and improves the table layout with better column styling. Additional changes include handling checkbox functionality, adding error handling, and enabling forced updates for re-fetching components.

--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from "react";
 
 interface ButtonProps {
   children: ReactNode; // Button text or content
-  size?: "sm" | "md"; // Button size
+  size?: "sm" | "md" | "xs"; // Button size
   variant?: "primary" | "outline"; // Button variant
   startIcon?: ReactNode; // Icon before the text
   endIcon?: ReactNode; // Icon after the text
@@ -23,8 +23,9 @@ const Button: React.FC<ButtonProps> = ({
 }) => {
   // Size Classes
   const sizeClasses = {
+    xs: "px-3 py-2 text-xs",
     sm: "px-4 py-3 text-sm",
-    md: "px-5 py-3.5 text-sm",
+    md: "px-5 py-3.5 text-md",
   };
 
   // Variant Classes
@@ -53,3 +54,5 @@ const Button: React.FC<ButtonProps> = ({
 };
 
 export default Button;
+
+
