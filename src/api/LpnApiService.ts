8a6a27dcec4798830193beac046b9a477e0dc152
THANHPC\Accou <nct031194@icloud.com>
2025-06-11 17:38:09 -0500

    Add API integration to fetch and display LPN data
    Replaced static row data with dynamic data fetched via API. Introduced `useEffect` to call `getAllLpn` and populate the grid with LPN data, improving data accuracy and maintainability. Also refined types and removed unused columns for cleaner implementation.

--- src/api/LpnApiService.ts
+++ src/api/LpnApiService.ts
@@ -1,9 +1,22 @@
-import {UserProp} from "../interfaces/User";
 import axiosInstance from "../utils/axiosInstance";
-import {LPNRequestProp} from "../interfaces/LPN";
+import {LPNProp, LPNRequestProp} from "../interfaces/LPN";
 
-export const createNewLpn = async (lpnRequestProp: LPNRequestProp): Promise<UserProp> => {
+export const createNewLpn = async (lpnRequestProp: LPNRequestProp): Promise<LPNRequestProp> => {
     const response = await axiosInstance.post(`/v1/lpn/new`, lpnRequestProp, {
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+
+    if (response.status !== 200) {
+        throw new Error(`${response.status}`);
+    }
+
+    return response.data;
+}
+
+export const getAllLpn = async (): Promise<LPNProp[]> => {
+    const response = await axiosInstance.get(`/v1/lpn`, {
         headers: {
             "Content-Type": "application/json",
         },
