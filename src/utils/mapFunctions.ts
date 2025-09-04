c4ae5de2c423865abb62d87c6c260dffaac19ae5
THANHPC\Accou <nct031194@icloud.com>
2025-09-04 13:38:31 -0500

    Add pricing fields and columns for product sheet
    Introduced new fields `ewfdirectPrice`, `ewfdirectManualPrice`, and `promotion` to support additional pricing details. Updated the ProductSheet table to include corresponding columns and enhanced mapping functions to handle these new fields.

--- src/utils/mapFunctions.ts
+++ src/utils/mapFunctions.ts
@@ -67,6 +67,7 @@ export const mapProductDetailPropToRequest = (
         components, // Include components from the input object
         ewfdirectManualPrice,
         promotion,
+        dimension
     } = product;
 
     return {
@@ -97,5 +98,6 @@ export const mapProductDetailPropToRequest = (
         components: mapComponentsToMinimalFormat(components), // Apply mapping to components
         ewfdirectManualPrice,
         promotion,
+        dimension
     };
 };
